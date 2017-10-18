6233cbe3d863db3c8b7afc02407e5f38f0dc8847
Michael Akanji <matscode@gmail.com>
2017-10-18 19:24:43 +0100

    Added UI assets/resources and creating a simple layout blade

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * Description
+ *
+ * @package        Consonance
+ * @category       Source
+ * @author         Michael Akanji <matscode@gmail.com> {@link http://michaelakanji.com}
+ * @date           2017-10-18
+ *
+ */
+
+?>
+
+<div class="alert alert-danger">
+
+	<div class="alert-title">{{ $title }}</div>
+
+	{{ $slot }}
+</div>
---
+++ resources/views/signup.blade.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * Description
+ *
+ * @package        Consonance
+ * @category       Source
+ * @author         Michael Akanji <matscode@gmail.com> {@link http://michaelakanji.com}
+ * @date           2017-10-18
+ *
+ */
+?>
+
+{{--use Default Layout--}}
+@extends('layouts.default')
+
+{{--set Title--}}
+@section('title', 'Signup')
+
+{{--set Header--}}
+@section('content')
+	<h1>Sign Up</h1>
+
+@endsection
