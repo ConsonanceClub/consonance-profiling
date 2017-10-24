dfdc2e7bed6df2ebc9c90bccccbf8b1afe3098cb
Michael Akanji <matscode@gmail.com>
2017-10-24 20:41:50 +0100

    DRY: Created Include-able Sections of a Layout

---
+++ resources/views/Elements/navigation.blade.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ *
+ * Description
+ *
+ * @package        Consonance
+ * @category       Source
+ * @author         Michael Akanji <matscode@gmail.com> {@link http://michaelakanji.com}
+ * @date           2017-10-22
+ *
+ */
+
+?>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+	<div class="container">
+
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+			        aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">
+				<img src="/images/logo-146x150.png" alt="Consonance">
+			</a>
+		</div>
+
+		{{--point of navbar collapse--}}
+		<div id="navbar" class="navbar-collapse collapse">
+
+			<div class="navbar-right">
+				<ul class="nav navbar-nav">
+					<li>
+						<a href="#">ACTIVITY</a>
+					</li>
+					<li>
+						<a href="#">BLOG</a>
+
+					</li>
+					<li>
+						<a href="#">GROUPS</a>
+					</li>
+					<li>
+						<a href="#">MEMBERS</a>
+					</li>
+					<li>
+						<a href="#">ABOUT US</a>
+					</li>
+				</ul>
+
+				<a class="btn navbar-btn">SIGN IN</a>
+			</div>
+
+		</div><!--/.navbar-collapse -->
+
+	</div>
+
+</nav>
