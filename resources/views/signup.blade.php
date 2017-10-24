b57528352dab91e219e33c08794004c0a9f5e521
Michael Akanji <matscode@gmail.com>
2017-10-24 20:44:22 +0100

    Sign Up Blade..

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -12,13 +12,45 @@
 ?>
 
 {{--use Default Layout--}}
-@extends('layouts.default')
+@extends('Layouts.default')
 
 {{--set Title--}}
 @section('title', 'Signup')
 
-{{--set Header--}}
+{{--set content--}}
 @section('content')
-	<h1>Sign Up</h1>
+
+	<div class="row">
+		<div class="col-sm-6">
+
+			<h3>Sign Up</h3>
+			<hr>
+			<form action="">
+				<div class="form-group">
+					<input type="text"
+					       name="email"
+					       placeholder="example@gmail.com"
+					       class="form-control input-lg">
+				</div>
+
+				<div class="form-group">
+					<input type="text"
+					       name="password"
+					       placeholder="e$r45EfnD$%"
+					       class="form-control input-lg">
+				</div>
+
+				<div class="form-group text-right">
+					<button class="btn btn-primary btn-lg">
+						LOG IN
+					</button>
+				</div>
+
+			</form>
+		</div>
+		<div class="col-sm-6"></div>
+	</div>
+
 
 @endsection
+
