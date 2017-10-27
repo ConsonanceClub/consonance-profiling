64ace8731806a41605725ca01ae411fe00d5735a
Michael Akanji <matscode@gmail.com>
2017-10-27 12:41:15 +0100

    Activity page completed...

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -19,6 +19,63 @@
 
 {{--set content--}}
 @section('content')
-	Content
+	<section id="activities">
+
+		<div class="row">
+
+			<div class="col-sm-6">
+
+				<div class="activity">
+					<div class="gfx img-responsive">
+						<img data-src="holder.js/270x150?random=yes" alt="Activity">
+						<span class="category">Software</span>
+					</div>
+					<h3 class="heading">
+						<a href="#"> The Big Bang Theory </a>
+					</h3>
+					<p class="summary">
+						kjhsbdfinoinsfd sdiufnsoiufn isufnoinsf isunfiulsf siufb sfiulnbsiuf siuliufnusinfved liaucfsniu
+						feiffdindiu iduiud iduvn dilvund vdsuliv dilundv ilunsdfv iundv iolndv isunondsv iulndv ilundv
+						ikjnviudsnvuid ionsf oidunf oiundf oif foifo voindvgjdn siunhs;pomfriv ojinfv ojuinedv jiondsvo
+						fjfnuisdf iufnisn siufnuois fiundv iln fv jdinbfv fiuonvdfvkd uoinv iunsbuoidnfoics diunvfv oif
+						dfhbidsfvio doiunv iounsfv jiounfuisdoinfv iunv oiunviodfnv iounsduinuief tiunbsudvnuindf isun
+						kjibdfivnbiusdfv inbvs diusoiufv isbnicundsr vioubdnsviuonancf divubis siufdnsfc fiunv dsciusfv
+						siunuisdnv iunfv iunvd diunbsac isua vfiunbv iufv siuv iuv diubdicunbosd viubvindsv ijunufiv di
+						sdfibsniuv iunvdf voiunsdvsinfv......
+					</p>
+				</div>
+			</div>
+
+			<div class="col-sm-6">
+				<div class="activity">
+					<div class="gfx img-responsive">
+						<img data-src="holder.js/270x150?random=yes" alt="Activity">
+						<span class="category">Software</span>
+					</div>
+					<h3 class="heading">
+						<a href="#"> Just a funny header, seemingly!!</a>
+					</h3>
+					<p class="summary">
+						kjhsbdfinoinsfd sdiufnsoiufn isufnoinsf isunfiulsf siufb sfiulnbsiuf siuliufnusinfved
+						liaucfsniu
+						feiffdindiu iduiud iduvn dilvund vdsuliv dilundv ilunsdfv iundv iolndv isunondsv iulndv
+						ilundv
+						ikjnviudsnvuid ionsf oidunf oiundf oif foifo voindvgjdn siunhs;pomfriv ojinfv ojuinedv
+						jiondsvo
+						fjfnuisdf iufnisn siufnuois fiundv iln fv jdinbfv fiuonvdfvkd uoinv iunsbuoidnfoics diunvfv
+						oif
+						dfhbidsfvio doiunv iounsfv jiounfuisdoinfv iunv oiunviodfnv iounsduinuief tiunbsudvnuindf
+						isun
+						kjibdfivnbiusdfv inbvs diusoiufv isbnicundsr vioubdnsviuonancf divubis siufdnsfc fiunv
+						dsciusfv
+						siunuisdnv iunfv iunvd diunbsac isua vfiunbv iufv siuv iuv diubdicunbosd viubvindsv ijunufiv
+						di
+						sdfibsniuv iunvdf voiunsdvsinfv......
+					</p>
+				</div>
+
+			</div>
+
+	</section>
 @endsection
 
