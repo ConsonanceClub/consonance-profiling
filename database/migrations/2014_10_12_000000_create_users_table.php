7ac757adbf6a4d091f464c40812108af5b2db3c5
Mayowa <mtudonu85@gmail.com>
2018-01-01 22:19:21 +0100

    Add default values to columns that will not be filled on first registration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,20 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
+            $table->string('username')->default();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('state');
-            $table->string('skills');
-            $table->string('short_bio', 1024);
-            $table->string('medium_username', 45);
-            $table->string('twitter_username', 45);
-            $table->string('github_username', 45);
-            $table->string('slack_username', 45);
-            $table->integer('school_id')->unsigned();
-            $table->tinyInteger('active');
+            $table->string('state')->default();
+            $table->string('skills')->default();
+            $table->string('short_bio', 1024)->default();
+            $table->string('medium_username', 45)->default();
+            $table->string('twitter_username', 45)->default();
+            $table->string('github_username', 45)->default();
+            $table->string('slack_username', 45)->default();
+            $table->integer('school_id')->unsigned()->default();
+            $table->tinyInteger('active')->default();
             $table->rememberToken();
             $table->timestamps();
             
