7d46613becc19127305e698f8d3c9814eee21e78
Mayowa <mtudonu85@gmail.com>
2018-01-01 22:18:27 +0100

    Remove Users table foreign key dependency on Schools table

--- database/migrations/2017_10_01_065515_add_fk_school_to_users_table.php
+++ database/migrations/2017_10_01_065515_add_fk_school_to_users_table.php
@@ -14,7 +14,7 @@ class AddFkSchoolToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreign('school_id')->references('id')->on('schools');
+            //$table->foreign('school_id')->references('id')->on('schools');
         });
     }
 
