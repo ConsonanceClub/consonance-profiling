d150b5b5c406e9bca7bf5184fb80cfd7d31ac20f
Mayowa <mtudonu85@gmail.com>
2018-01-02 20:23:58 +0100

    Remove sign-in from Registration view

--- resources/views/signUp.blade.php
+++ resources/views/signUp.blade.php
@@ -20,68 +20,7 @@
 {{--set content--}}
 @section('content')
     <div class="row">
-        <div class="col-sm-offset-1 col-sm-5">
 
-            <div class="padding-20">
-                <h3 class="text-bold">Sign In</h3>
-
-                <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                    {{ csrf_field() }}
-
-
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-
-                        <input id="sign-email"
-                               type="email"
-                               placeholder="Email"
-                               class="form-control input-lg"
-                               name="email"
-                               value="{{ old('email') }}" required autofocus>
-
-                        @if ($errors->has('email'))
-                            <span class="help-block">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                        @endif
-                    </div>
-
-
-                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-
-                        <input id="sign-password"
-                               type="password"
-                               class="form-control input-lg"
-                               name="password" required>
-
-                        @if ($errors->has('password'))
-                            <span class="help-block">
-									<strong>{{ $errors->first('password') }}</strong>
-								</span>
-                        @endif
-                    </div>
-
-
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me?
-                        </label>
-                    </div>
-
-
-                    <div class="form-group text-right">
-                        <button class="btn btn-primary" type="submit">
-                            Sign In
-                        </button>
-
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            Forgot Your Password?
-                        </a>
-                    </div>
-
-                </form>
-            </div>
-
-        </div>
 
             <div class="col-sm-5">
 
