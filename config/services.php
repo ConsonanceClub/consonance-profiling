c33c63112b203c85f85ed585ac9506152b1141d1
Mayowa <mtudonu85@gmail.com>
2018-01-06 14:49:24 +0100

    Change callack url for git credentials

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://your-callback-url',
+        'redirect' => 'http://localhost:9000/home',
     ],
 
 ];
