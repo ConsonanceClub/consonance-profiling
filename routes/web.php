145a66dc4fda760929761a9cad747e93ead208bc
Mayowa <mtudonu85@gmail.com>
2018-01-06 18:43:40 +0100

    Add group middleware for routes

--- routes/web.php
+++ routes/web.php
@@ -12,49 +12,55 @@
 */
 use Illuminate\Http\Request;
 
-Route::get('/sign-in', function (){
-    return view('auth.register');
+
+Route::group(['midleware' => ['web']], function (){
+
+    Route::get('/sign-in', function (){
+        return view('auth.register');
+    });
+
+    Route::get('/', function () {
+        return view('signIn');
+    });
+
+    Route::get('/member/sign-in',[ 'as'=>'member.sign-in', function () {
+        return view('signIn');
+    }]);
+
+    Route::get('/member/sign-up',[ 'as'=>'member.sign-up', function () {
+        return view('signUp');
+    }]);
+
+    Route::get('/activity', function () {
+        return view('activity');
+    });
+
+    Route::get('/members', function () {
+        return view('members');
+    });
+
+    Route::get('/getMembers', 'MembersController@getAllMembers');
+
+    Auth::routes();
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+
+    Route::get('auth/login/{driver}',[
+        'uses' => 'Auth\LoginController@redirectToProvider',
+        'as' => 'social.auth'
+    ]);
+
+    Route::get('auth/login/{driver}/callback',[
+        'uses' => 'Auth\LoginController@handleProviderCallback',
+        'as' => 'social.callback'
+    ]);
+
+
+    Route::get('/welcome/member', function (){
+        return view('welcome');
+    });
 });
 
-Route::get('/', function () {
-    return view('signIn');
-});
 
-Route::get('/member/sign-in',[ 'as'=>'member.sign-in', function () {
-    return view('signIn');
-}]);
-
-Route::get('/member/sign-up',[ 'as'=>'member.sign-up', function () {
-    return view('signUp');
-}]);
-
-Route::get('/activity', function () {
-    return view('activity');
-});
-
-Route::get('/members', function () {
-    return view('members');
-});
-
-Route::get('/getMembers', 'MembersController@getAllMembers');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
-Route::get('auth/login/{driver}',[
-    'uses' => 'Auth\LoginController@redirectToProvider',
-    'as' => 'social.auth'
-]);
-
-Route::get('auth/login/{driver}/callback',[
-    'uses' => 'Auth\LoginController@handleProviderCallback',
-    'as' => 'social.callback'
-]);
-
-
-Route::get('/welcome/member', function (){
-    return view('welcome');
-});
 
