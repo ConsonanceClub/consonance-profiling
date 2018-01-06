40f74be0d6ab7d000ca4b5b3e86684cb12a68d74
Mayowa <mtudonu85@gmail.com>
2018-01-06 20:17:55 +0100

    Add new Social auth controller

---
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SocialAuthController extends Controller
+{
+    //
+    public function execute(){
+        return 'Hey';
+    }
+}
