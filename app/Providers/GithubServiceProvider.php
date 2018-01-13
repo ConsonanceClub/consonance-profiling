fd979552d3c3b57fb8ef8e2a7064c136a8c28c69
Mayowa <mtudonu85@gmail.com>
2018-01-13 17:50:35 +0100

    Add implementation for github auth

--- app/Providers/GithubServiceProvider.php
+++ app/Providers/GithubServiceProvider.php
@@ -12,4 +12,50 @@ namespace app\Providers;
 class GithubServiceProvider
 {
 
+    private $firstName;
+    private $lastName;
+    private $userName;
+    private $email;
+    private $bio;
+    private $url;
+    private $avatar;
+
+    public function __construct($user)
+    {
+        $user = $user->user;
+        $this->firstName = explode(' ',$user['name'])[0];
+        $this->lastName = explode(' ',$user['name'])[1];
+        $this->email = $user['email'];
+        $this->url = $user['url'];
+        $this->avatar = $user['avatar_url'];
+        $this->bio = $user['bio'];
+    }
+
+    public function getFirstName(){
+        return $this->firstName;
+    }
+
+    public function getLastName(){
+        return $this->lastName;
+    }
+
+    public function getUserName(){
+        return $this->userName;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function getBio(){
+        return $this->bio;
+    }
+
+    public function getUrl(){
+        return $this->url;
+    }
+
+    public function getAvatar(){
+        return $this->avatar;
+    }
 }
