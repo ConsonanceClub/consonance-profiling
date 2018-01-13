6674e428898d416f985e681c86e25a6c784eee63
Mayowa <mtudonu85@gmail.com>
2018-01-13 19:40:50 +0100

    Add linkeIn to drivers list

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Auth\SociallyAuthenticateUser;
 
 class SocialAuthController extends Controller
 {
-    //
-    public function execute(){
-        return 'Hey';
+    //social auth drivers
+    protected $drivers = ['github', 'twitter', 'linkedin'];
+
+    public function execute($driver, Request $request){
+        if(!$this->checkDriver($driver))
+            return response('Page not found', 404);
+
+        $authUser = new SociallyAuthenticateUser();
+        $authUser->driver = $driver;
+
+        $user =\Socialite::driver($driver)->user();
+        dump ($user->user);
+        //return $authUser->execute($request);
+    }
+
+    private function checkDriver($driver){
+        if(in_array($driver,$this->drivers))
+            return true;
+        return false;
     }
 }
