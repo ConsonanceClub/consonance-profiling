22abe63f01f02cc9197d04eeb65e0853bcd82a2c
Mayowa <mtudonu85@gmail.com>
2018-01-13 20:20:49 +0100

    Add social parent

--- app/Providers/TwitterServiceProvider.php
+++ app/Providers/TwitterServiceProvider.php
@@ -9,55 +9,10 @@
 namespace app\Providers;
 
 
-class TwitterServiceProvider
+use App\Models\Social;
+
+class TwitterServiceProvider extends Social
 {
 
-    private $firstName;
-    private $lastName;
-    private $userName;
-    private $email;
-    private $bio;
-    private $url;
-    private $avatar;
-
-    public function __construct($user)
-    {
-        $user = $user->user;
-        $this->firstName = explode(' ',$user['name'])[0];
-        $this->lastName = explode(' ',$user['name'])[1];
-        $this->userName = $user['username'];
-        $this->email = $user['email'];
-        $this->url = $user['url'];
-        $this->avatar = $user['avatar_url'];
-        $this->bio = $user['bio'];
-    }
-
-    public function getFirstName(){
-        return $this->firstName;
-    }
-
-    public function getLastName(){
-        return $this->lastName;
-    }
-
-    public function getUserName(){
-        return $this->userName;
-    }
-
-    public function getEmail(){
-        return $this->email;
-    }
-
-    public function getBio(){
-        return $this->bio;
-    }
-
-    public function getUrl(){
-        return $this->url;
-    }
-
-    public function getAvatar(){
-        return $this->avatar;
-    }
 
 }
