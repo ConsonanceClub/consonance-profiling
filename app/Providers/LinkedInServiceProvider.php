abb0a77fd4496fa6a95928f61127df6d1366a23e
Mayowa <mtudonu85@gmail.com>
2018-01-13 19:47:40 +0100

    Add linkedin service provider

---
+++ app/Providers/LinkedInServiceProvider.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 1/13/18
+ * Time: 7:45 PM
+ */
+
+namespace app\Providers;
+
+
+class LinkedInServiceProvider
+{
+
+    private $firstName;
+    private $lastName;
+    private $userName;
+    private $email;
+    private $bio;
+    private $url;
+    private $avatar;
+
+    public function __construct($user)
+    {
+        $user = $user->user;
+        $this->firstName = explode(' ',$user['name'])[0];
+        $this->lastName = explode(' ',$user['name'])[1];
+        $this->userName = $user['username'];
+        $this->email = $user['email'];
+        $this->url = $user['url'];
+        $this->avatar = $user['avatar_url'];
+        $this->bio = $user['bio'];
+    }
+
+    public function getFirstName(){
+        return $this->firstName;
+    }
+
+    public function getLastName(){
+        return $this->lastName;
+    }
+
+    public function getUserName(){
+        return $this->userName;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function getBio(){
+        return $this->bio;
+    }
+
+    public function getUrl(){
+        return $this->url;
+    }
+
+    public function getAvatar(){
+        return $this->avatar;
+    }
+}
