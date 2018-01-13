94a5cd82a4137ee5a20b6c5b4400910e93b1c553
Mayowa <mtudonu85@gmail.com>
2018-01-13 12:40:10 +0100

    Add functionalities for social auth execute

--- app/Http/Controllers/Auth/SociallyAuthenticateUser.php
+++ app/Http/Controllers/Auth/SociallyAuthenticateUser.php
@@ -7,9 +7,33 @@
  */
 
 namespace App\Http\Controllers\Auth;
-
+use Illuminate\Http\Request;
+use Laravel\Socialite\Two\InvalidStateException;
+use Mockery\Exception;
+use Socialite;
 
 class SociallyAuthenticateUser
 {
+    public $driver;
 
+    public function execute($request = null){
+
+        if($request == null)
+            return $this->needsAuthentication();
+        try{
+            $user = Socialite::driver($this->driver)->user;
+            return $user;
+
+        }catch (InvalidStateException $exception){
+            return redirect(`auth/login/{$this->driver}`);
+        }catch (Exception $exception){
+            return redirect('/');
+        }
+
+        return '';
+    }
+
+    private function needsAuthentication(){
+        return '';
+    }
 }
