0282a447b42fa7b74876f833a6b40ed7620f8341
ashinzekene <ashinzekene@gmail.com>
2018-02-22 00:24:29 +0100

    used blade for loop

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -19,6 +19,7 @@
     <div class="container-fluid">
         <h1>View Our Projects</h1>
         <div class="row project-list">
+            @for ($i = 0; $i < 12; $i++)
             <div class="col-lg-4 col-md-6 col-sm-12 project">
                 <div class="card">
                     <img src="/images/logo-375x388.png" alt="project name" srcset="" class="card-image">
@@ -33,48 +34,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 project">
-                <div class="card">
-                    <img src="/images/logo-375x388.png" alt="project name" srcset="" class="card-image">
-                    <h2 class="card-header">Default Project</h2>
-                    <div class="card-body">
-                        <div class="tags">
-                            <span class="label label-primary">CSS</span>
-                            <span class="label label-warning">JavaScript</span>
-                            <span class="label label-danger">Python</span>
-                        </div>
-                        This is a secure voting platform for creating polls and for authenticating transactions using blockchain technology  
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 project">
-                <div class="card">
-                    <img src="/images/logo-375x388.png" alt="project name" srcset="" class="card-image">
-                    <h2 class="card-header">Default Project</h2>
-                    <div class="card-body">
-                        <div class="tags">
-                            <span class="label label-primary">CSS</span>
-                            <span class="label label-warning">JavaScript</span>
-                            <span class="label label-danger">Python</span>
-                        </div>
-                        This is a secure voting platform for creating polls and for authenticating transactions using blockchain technology  
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 project">
-                <div class="card">
-                    <img src="/images/logo-375x388.png" alt="project name" srcset="" class="card-image">
-                    <h2 class="card-header">Default Project</h2>
-                    <div class="card-body">
-                        <div class="tags">
-                            <span class="label label-primary">CSS</span>
-                            <span class="label label-warning">JavaScript</span>
-                            <span class="label label-danger">Python</span>
-                        </div>
-                        This is a secure voting platform for creating polls and for authenticating transactions using blockchain technology  
-                    </div>
-                </div>
-            </div>
+            @endfor
         </div>
     </div>
     @endsection
