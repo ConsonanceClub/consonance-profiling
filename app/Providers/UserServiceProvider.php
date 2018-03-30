844c533b1489a57c37113a53688552a6eef81abd
Mayowa <mtudonu85@gmail.com>
2018-03-30 21:55:48 +0100

    Change access methods for create user function

--- app/Providers/UserServiceProvider.php
+++ app/Providers/UserServiceProvider.php
@@ -8,34 +8,48 @@
 
 namespace app\Providers;
 
+use app\Models\Social;
 use App\Providers\SocialServiceProvider;
 use App\User;
 
 class UserServiceProvider
 {
 
-    public static function createUser($userDetails, $driver){
+    private static function createUser(Social $socialDetails, $driver){
         $user = new User();
-        $user->first_name = $userDetails['first_name'];
-        $user->last_name = $userDetails['last_name'];
-        $user->short_bio = $userDetails['bio'];
-        $user->email = $userDetails['email'];
-        $user->username = $userDetails['username'];
+        $user->first_name = $socialDetails->getFirstName();
+        $user->last_name = $socialDetails->getLastName();
+        $user->short_bio = $socialDetails->getBio();
+        $user->email = $socialDetails->getEmail();
+        $user->username = $socialDetails->getUserName();
         if($driver == 'github'){
-            $user->github_url = $userDetails['github_url'];
+            $user->github_url = $socialDetails->getUrl();
         }else if($driver == 'twitter'){
-            $user->twitter_url = $userDetails['twitter_url'];
+            $user->twitter_url = $socialDetails->getUrl();
         }else if($driver == 'linkedin'){
-            $user->linkedin_url = $userDetails['linkedin_url'];
+            $user->linkedin_url = $socialDetails->getUrl();
         }
-        $user->avatar = $userDetails['avatar'];
+        $user->avatar = $socialDetails->getAvatar();
 
         $user->save();
         return $user;
     }
 
-    public static function userExists($userDetails){
+    public static function getUserDetails($user, $driver){
 
-        return true;
+        $socialDetails = SocialServiceProvider::getSocialProvider($driver,$user);
+
+        $email = $socialDetails->getEmail();
+        if(!is_null($email)){
+            $existingUser = User::where('email', $email)->first();
+        }else $existingUser = null;
+
+        if($existingUser instanceof User){
+            return $existingUser;
+        }{
+            $existingUser = static::createUser($socialDetails, $driver);
+        }
+
+        return $existingUser;
     }
 }
