15c96f44338d5eb4f28c279435f79e12d8d3de92
Mayowa <mtudonu85@gmail.com>
2018-03-30 22:15:23 +0100

    Add core functionalities to social controller

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -11,16 +11,27 @@ class SocialAuthController extends Controller
     //social auth drivers
     protected $drivers = ['github', 'twitter', 'linkedin'];
 
-    public function execute($driver, Request $request){
+    public function login($driver, Request $request){
         if(!$this->checkDriver($driver))
             return response('Page not found', 404);
 
         $authUser = new SociallyAuthenticateUser();
         $authUser->driver = $driver;
+        return $authUser->execute($request);
+    }
 
-        $user =\Socialite::driver($driver)->user();
-        dump ($user->user);
-        //return $authUser->execute($request);
+    public function loginUserViaDriver($driver, Request $request){
+        if(!$this->checkDriver($driver))
+            return response('Page not found', 404);
+
+        $authUser = new SociallyAuthenticateUser();
+        $authUser->driver = $driver;
+        return $authUser->execute();
+    }
+
+    public function logout(){
+        Auth::logout();
+        return Redirect::route('/');
     }
 
     private function checkDriver($driver){
