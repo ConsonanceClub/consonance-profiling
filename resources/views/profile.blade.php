290478048ea94c9f118626a464406765e3b520ec
Mayowa <mtudonu85@gmail.com>
2018-03-31 09:35:59 +0100

    Add changes to profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -27,23 +27,31 @@
                 <h3 class="text-white text-uppercase"><strong>Personal Information</strong></h3>
                 <div class="row">
                     <div class="p-info form-group col-lg-12">
-                        <input type="text" placeholder="John Doe" class="form-control text-center padding-20">
+                        <input type="text" placeholder="John Doe" value="{{Auth::user()->first_name . ' ' .Auth::user()->last_name  }}"
+                               class="form-control text-center padding-20">
                     </div>
                     <div class="p-info form-group col-lg-12">
-                        <input type="text" placeholder="Position" class="form-control text-center padding-20">
+                        @if(Auth::user()->role_id == 1)
+                            <input type="text" placeholder="Position" value="Admin" class="form-control text-center padding-20">
+                        @else
+                            <input type="text" placeholder="Position" value="Member" class="form-control text-center padding-20">
+                        @endif
+
                     </div>
                     <div class="p-info form-group col-lg-12">
-                        <input type="text" placeholder="Community Role" class="form-control text-center padding-20">
+                        <input type="text" placeholder="Community Role" value="Active Contributor" class="form-control text-center padding-20">
                     </div>
                     <div class="p-info form-group col-lg-12">
-                        <input type="text" placeholder="Conact Info" class="form-control text-center padding-20">
+                        <input type="text" placeholder="Conact Info" value="{{Auth::user()->email}}" class="form-control text-center padding-20">
                     </div>
+                    <!--
                     <div class="p-info form-group col-lg-12">
                         <input type="text" placeholder="Projects" class="form-control text-center padding-20">
                     </div>
                     <div class="p-info form-group col-lg-12">
                         <input type="text" placeholder="About Me" class="form-control text-center padding-20">
                     </div>
+                    -->
                 </div>
 		</div>
 
@@ -51,8 +59,7 @@
 		<div class="col-sm-6 col-md-4">
             <h3 class="text-white text-uppercase"><strong>BIO</strong></h3>
             <div class="bio-text">
-                <textarea class="form-control" name="bio" id="profile-bio" cols="30" rows="10">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex delectus quasi qui, exercitationem quod eos suscipit rerum dolores illum repellat dolore ad. Ducimus voluptate, dolorem excepturi esse cumque autem atque.Sunt ad praesentium quasi ut saepe aut. Sint, cum nostrum dolores deserunt tempore perferendis exercitationem aspernatur eaque ratione! Fugit enim quos in vitae eos facilis assumenda officiis beatae, voluptatum eveniet.
+                <textarea class="form-control" name="bio" id="profile-bio" cols="30" rows="10">{{Auth::user()->short_bio}}
                 </textarea>
             </div>
             <h3 class="text-white text-uppercase"><strong>SCHOOL</strong></h3>
