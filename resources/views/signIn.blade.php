6228a32e215e7bfa4c02802a8237162500578e0f
Mayowa <mtudonu85@gmail.com>
2018-03-31 12:40:09 +0100

    Comment out twitter signin

--- resources/views/signIn.blade.php
+++ resources/views/signIn.blade.php
@@ -95,9 +95,11 @@
 				<a class="navbar-brand" href="{{route('social.auth', ['driver' => 'github'])}}">
 					<img src="/images/GitHub-Mark-32px.png" alt="Consonance">
 				</a>
+				<!--
 				<a class="navbar-brand" href="{{route('social.auth', ['driver' => 'twitter'])}}">
 					<img src="/images/Twitter_Social_Icon_Circle_Color.png" alt="Consonance">
 				</a>
+				-->
 				<a class="navbar-brand" href="{{route('social.auth', ['driver' => 'linkedin'])}}">
 					<img src="/images/Logo-2C-28px-TM.png" alt="Consonance">
 				</a>
