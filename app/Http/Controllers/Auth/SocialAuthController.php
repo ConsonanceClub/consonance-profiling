3de8673fef5823ffccf924f8233a3287e84c159b
Mayowa <mtudonu85@gmail.com>
2018-03-31 01:05:34 +0100

    Add Auth class Remove unused imports

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Auth\SociallyAuthenticateUser;
+use Auth;
 
 class SocialAuthController extends Controller
 {
