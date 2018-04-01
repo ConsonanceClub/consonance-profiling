4fcbdaa9a97ccf7875fbfb0c95acb56912df89e6
Mayowa <mtudonu85@gmail.com>
2018-04-01 04:43:49 +0100

    Fix update function

--- public/js/components/members.js
+++ public/js/components/members.js
@@ -23,3 +23,31 @@ function loadMembers() {
     })
 }
 loadMembers();
+
+function updateProfile() {
+
+    const interests = $('.interest-cbox:checkbox');
+    const name = $('#name').val();
+    const email = $('#email').val();
+    const bio = $('#profile-bio').val();
+    const school = $('#school').val();
+    const checkedInterests = {};
+
+
+    for(let i = 0; i < interests.length; i++){
+        if(interests[i].checked){
+            checkedInterests[interests[i].id] = true;
+        }else{
+            checkedInterests[interests[i].id] = false;
+        }
+    }
+
+    $.ajax({
+        url:'/update-profile',
+        type:'GET',
+        data:{interests:checkedInterests, name:name, bio:bio, email:email, school:school},
+        success:function (data) {
+            const result = JSON.parse(data);
+        }
+    });
+}
