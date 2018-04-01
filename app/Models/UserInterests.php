4bbe729344944a69c70f054ddd0daf2725da2944
Mayowa <mtudonu85@gmail.com>
2018-04-01 03:48:18 +0100

    Add UserInterest Model to Models

--- app/Models/UserInterests.php
+++ app/Models/UserInterests.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,7 +11,7 @@ class UserInterests extends Model
     protected $table = 'users_interests';
 
     protected $fillable = [
-      'email',
-      'interest'
+        'email',
+        'interest'
     ];
 }
