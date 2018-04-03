d42223d96cee073fb914e96136da542bc579e5d3
Ashinzekene <ashinzekene@gmail.com>
2018-04-03 18:14:12 +0100

    worked on projects page

--- resources/views/project-old.blade.php
+++ resources/views/project-old.blade.php
@@ -53,10 +53,10 @@
             </div>
             <h1>View Our Projects</h1>
             <div class="row project-list">
-                @for ($i = 0; $i < 12; $i++) 
-                <div class="col-lg-4 col-md-6 col-sm-12 a-project">
-                    <div class="project-card">
-                        <img src="/images/about-video.jpg" alt="project name" srcset="" class="card-image">
+                @for ($i = 0; $i
+                < 12; $i++) <div class="col-lg-4 col-md-6 col-sm-12 project">
+                    <div class="card">
+                        <img src="/images/logo-375x388.png" alt="project name" srcset="" class="card-image">
                         <h2 class="card-header">Default Project</h2>
                         <div class="card-body">
                             <div class="tags">
