0af9ec466393e61300e96424d5517a5886629771
Mayowa <mtudonu85@gmail.com>
2018-04-08 19:44:28 +0100

    Fix redirect route

--- app/Http/Controllers/Auth/SociallyAuthenticateUser.php
+++ app/Http/Controllers/Auth/SociallyAuthenticateUser.php
@@ -32,7 +32,7 @@ class SociallyAuthenticateUser
             }
 
         }catch (InvalidStateException $exception){
-            return redirect(`auth/login/{$this->driver}`);
+            return redirect()->route(`auth/login/{$this->driver}`);
         }catch (Exception $exception){
             return redirect()->route('signIn');
         }
