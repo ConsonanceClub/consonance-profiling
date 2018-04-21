7af446044a6df1561bbccc7aefdb98341292999d
Ashinzekene <ashinzekene@gmail.com>
2018-04-21 20:05:19 +0100

    fixed navbar issue

--- resources/views/Elements/navigation.blade.php
+++ resources/views/Elements/navigation.blade.php
@@ -11,7 +11,7 @@
       <section class="menu-list-items">
         <div class="container">
           <div class="row">
-            <div class="col-lg-12 col-md-12">
+            <div class="col-lg-12 col-md-12 header-div">
               <!-- menu logo -->
               <ul class="menu-logo col-md-3">
                 <li>
