4beb2984bca1f193b66925248f9d7aecc0f234ed
Mayowa <mtudonu85@gmail.com>
2018-04-26 12:12:27 +0100

    Remove unused function from code

--- public/js/components/members.js
+++ public/js/components/members.js
@@ -1,29 +1,5 @@
 'use strict';
 
-function loadMembers() {
-    $(document).ready(function () {
-        $('#members').DataTable({
-
-            'ajax':'/getMembers',
-            'columns':[
-                {"data":"first_name"},
-                {"data":"last_name"},
-                {"data":"email"},
-                {"data":"state"},
-                {"data":"school.name"},
-                {'data':'skills'},
-                {"data":"medium_username"},
-                {"data":"slack_username"},
-                {"data":"twitter_username"},
-                {"data":"github_username"},
-            ]
-        });
-        $('#members_length').css('padding-left','10px');
-        $('#members_filter').css('padding-right', '10px');
-    })
-}
-loadMembers();
-
 function updateProfile() {
 
     const interests = $('.interest-cbox:checkbox');
