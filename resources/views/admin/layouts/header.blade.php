4f71be7441d299b7491180e1705afbafde82a3d0
Mayowa <mtudonu85@gmail.com>
2018-04-28 23:21:37 +0100

    Remove unused file

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -16,8 +16,7 @@
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
 
-                {{----}}
-                <!-- User Account: style can be found in dropdown.less -->
+
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="{{asset('images/logo-146x150.png')}}" class="user-image" alt="User Image">
