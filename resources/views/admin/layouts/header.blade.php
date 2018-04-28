97ed18aa71714eeb6676b8b1299160c1024326b7
Mayowa <mtudonu85@gmail.com>
2018-04-28 23:32:49 +0100

    Refactor code

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -36,9 +36,7 @@
 
                         <!-- Menu Footer-->
                         <li class="user-footer">
-                            {{--<div class="pull-left">--}}
-                                {{--<a href="#" class="btn btn-default btn-flat">Profile</a>--}}
-                            {{--</div>--}}
+
                             <div class="pull-right">
                                 <a class="btn btn-default btn-flat"  href="{{ route('auth.logout') }}"
                                onclick="event.preventDefault();
